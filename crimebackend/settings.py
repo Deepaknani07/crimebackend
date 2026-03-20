6a83e70f228f21b3a3a266fcd54efc180dafe5af
DEEPAK P <nanideepak05@gmail.com>
2026-03-20 20:58:24 +0530

    Update settings.py to include STATIC_ROOT and allow all hosts; add Procfile for Gunicorn deployment

--- crimebackend/settings.py
+++ crimebackend/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ALLOWED_HOSTS = ['*']
